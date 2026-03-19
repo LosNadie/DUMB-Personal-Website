fca1c25238513f6094a51c80bdd1afcd654a3eba
LosNadie <2423572157@qq,com>
2026-03-20 01:27:15 +0800

    add oauth

--- api/oauth/authorize.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-    const client_id = process.env.GITHUB_CLIENT_ID;
-    const redirect_uri = `${process.env.BASE_URL}/api/oauth/callback`;
-  
-    const githubAuthURL = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=repo`;
-  
-    res.redirect(githubAuthURL);
-  }
